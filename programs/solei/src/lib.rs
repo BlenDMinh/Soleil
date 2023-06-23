e6496d3d53e40719bebf79914b08959001bf6f34
chouchou <chaudiemhoang2@gmail.com>
2023-06-23 12:48:36 +0700

    Update lib.rs

--- programs/solei/src/lib.rs
+++ programs/solei/src/lib.rs
@@ -12,11 +12,52 @@ pub mod solei {
         msg!("hello 123");
         Ok(())
     }
+
+    pub fn create_user(ctx: Context<CreateUser>, name: String, avatarImage: String) -> Result<()> {
+        let user_account = &mut ctx.accounts.user_account;
+        let authority = &mut ctx.accounts.authority;
+        user_account.name = name;
+        user_account.avatarImage = avatarImage;
+        user_account.authority = authority.key();
+        Ok(())
+    }
 }
 
+#[derive(Accounts)]
+#[instruction()]
+pub struct CreateUser<'info> {
+    #[account(
+        init,
+        seeds = [USER_SEED, authority.key().as_ref()],
+        payer = authority,
+        bump,
+        space = 2500
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>
+}
+
+// Instruction
 #[derive(Accounts)]
 pub struct Solei<'info> {
     #[account(mut)]
     payer: Signer<'info>,
     system_program: Program<'info, System>,
 }
+
+// State
+#[account]
+pub struct UserAccount {
+    name: String,
+    avatarImage: String,
+    authority: Pubkey
+}
+
+// Const
+
+#[constant]
+pub const USER_SEED: &[u8] = b"soleil_user";
